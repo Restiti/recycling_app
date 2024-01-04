5206f50bd4885646c18b9634eba854592b4734fb
remy <rem59570@gmail.com>
2024-01-04 22:11:44 +0100

    Init project

---
+++ src/test/java/com/fisa/recycling_app/RecyclingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.recycling_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
